cf466c45646e92dcb719eca1f9cf2871fa237e5d
kimdongkyu <dkkim1005@naver.com>
2017-09-05 11:26:25 +0900

    ---

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -3,10 +3,12 @@
 #include <cmath>
 #include <assert.h>
 #include <fstream>
+#include <numeric>
 
 // compile : g++ -o filename hw1.cpp -std=c++14 -lopenblas -llapack
 
-namespace LAPACK_SOLVER{
+namespace LAPACK_SOLVER
+{
 
 #define MAX(a, b) a>b ? a : b
 
@@ -58,15 +60,66 @@ namespace LAPACK_SOLVER{
 }
 
 
+namespace NUMERIC_CALCULUS 
+{
+	template<class T1, class T2>
+	T1 simpson_integration(const T1* psi, const int N, const T2 dx)
+	{
+		assert(N%2 == 1);
+
+		T1 accum = 0;
+
+		for(int i=1;i<N-1; i+=2) {
+			accum += 4.*psi[i];
+		}
+		for(int i=2;i<N-2; i+=2) {
+			accum += 2.*psi[i];
+		}
+
+		accum *= dx/3.;
+
+		return accum;
+	}
+} 
+
+
 void set_sign_eigen(std::vector<double>& A, const double sign = 1.)
 {
 	const int N = std::sqrt(A.size());
 	for(int j=0; j<N; ++j)
 	{
-		if(A[j]*sign < 0) {
+		if(A[j]*sign < 0) 
+		{
 			for(int i=0; i<N; ++i) {
 				A[N*i + j] *= -1;
 			}
+		}
+	}
+}
+
+
+void set_norm(std::vector<double>& A, const double a)
+{
+	const int N = std::sqrt(A.size()) + 2;
+	const double dx = a/(N-1.);
+	std::vector<double> x(N);
+
+	for(int i=0; i<N; ++i) {
+		x[i] = i*dx;
+	}
+
+	std::vector<double> psiSquare(N, 0);
+
+	for(int j=0; j<N-2; ++j) 
+	{
+		for(int i=1; i<N-1; ++i) {
+			psiSquare[i] = std::pow(A[(N-2)*(i-1)+ j], 2);
+		}
+
+		double norm = NUMERIC_CALCULUS::simpson_integration(&psiSquare[0], N, dx);
+
+		for(int i=1; i<N-1; ++i) {
+			A[(N-2)*(i-1)+ j] /= std::sqrt(norm);
 		}
 	}
 }
@@ -76,16 +129,15 @@ int main(int argc, char* argv[])
 {
 	// The number of width for discritization.
 	const int N = std::atoi(argv[1]);
+	assert(N%2 == 1);
 	// open a file object to record numeric solutions.
 	std::ofstream outFile(argv[2]);
 	assert(outFile.is_open());
-
 	const double HBAR = 6.62607004e-34/(2*M_PI),
 			M = 9.10938356e-31,
-			a = 1.;
+			a = 1e-10;
 	const double da = a/(N - 1.);
-	//const double coeff = -std::pow(HBAR, 2)/(2.*M*std::pow(da, 2));
-	const double coeff = -1./(2.*M*std::pow(da, 2));
+	const double coeff = -std::pow(HBAR, 2)/(2.*M*std::pow(da, 2));
 
 	std::vector<double> A(std::pow(N-2, 2), 0), lambda(N-2);
 
@@ -101,6 +153,9 @@ int main(int argc, char* argv[])
 	LAPACK_SOLVER::diag(A, lambda);
 
 	set_sign_eigen(A);
+
+	// nomalizaing the wave functions
+	set_norm(A, a);
 
 	// x : 0 to a
 	std::vector<double> x(N);
--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python2.7
 import numpy as np
+from scipy.integrate import simps 
 
 #Eigen solver for solving the Schrodinger equation in 1-D infinite potential wall.
 hbar = 6.62607004e-34/(2*np.pi) #(m^2 kg / s)
 m    = 9.10938356e-31 #(kg)
-N    = 100
 a    = 1e-10
+N    = 100
 da   = a/(N - 1.) # da = a/(N-1)
 coef = -hbar**2/(2*m*da**2) # -h^2/2m
 
@@ -31,10 +32,22 @@ psi[1:-1, :] = eig
 
 data = np.zeros([N, N-1], dtype = 'float64')
 data[:, 0] = x
-data[:, 1:] = np.abs(psi)
+data[:, 1:] = psi
+
+# normalizing the wave function
+for i in xrange(1, N-1):
+    norm = simps(data[:, i]**2, x)
+    data[:, i] /= np.sqrt(norm)
 
 np.savetxt('result.dat', data)
 
 print "Eigen energy:"
 print E[:10], "..."
 print E[-10:]
+
+k = np.sqrt(2.*m*E)/hbar
+exact_data = np.zeros([N, N-1], dtype = 'float64')
+exact_data[:, 0] = x
+for i in range(N):
+    exact_data[i, 1:] = np.sin(k*x[i])*np.sqrt(2./a)
+np.savetxt('exact.dat', exact_data)
