d59524ffc7c5fa314f35ba1185bad07c145a4f16
Dongkyu_Kim <dkkim1005@naver.com>
2017-09-12 19:31:16 +0900

    ---

---
+++ hw3/pyplot.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python2.7
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+
+def set_legend_fontsize(legend):
+    # Set the fontsize
+    for label in legend.get_texts():
+        label.set_fontsize(2)
+
+    for label in legend.get_lines():
+        label.set_linewidth(1.0)  # the legend line width
+
+def set_xlim(xmin, xmax):
+    axes = plt.gca()
+    axes.set_xlim([xmin, xmax])
+
+def set_ylim(ymin, ymax):
+    axes = plt.gca()
+    axes.set_ylim([ymin, ymax])
+
+pi = np.loadtxt("pi-Nplus.dat")
+
+# --: dashes,  s: squares  ^: triangles
+
+fig = plt.figure(1)
+
+plt.plot(pi [:, 0], pi [:, 1], 'ro-', label =  "" , markersize = 3) 
+
+plt.xlabel(r"""$N^{+}$(m)""", fontsize = 25)
+plt.xticks(fontsize = 25)
+#plt.xticks([2.0, 2.5, 3.0])
+
+plt.ylabel(r"""$\phi$""", fontsize = 25)
+plt.yticks(fontsize = 25)
+#plt.yticks([0, 5, 10, 15, 20])
+
+#legend = plt.legend(loc = 'best')
+#set_legend_fontsize(legend)
+
+plt.ticklabel_format(style='sci', axis = 'both', scilimits=(0, 0))
+
+plt.show()
