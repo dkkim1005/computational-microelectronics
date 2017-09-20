55e10b11e7e63ff01a0077018337dceb566b011a
kimdongkyu <dkkim1005@naver.com>
2017-09-20 21:44:45 +0900

    ---

--- hw5/hw5.cpp
+++ hw5/hw5.cpp
@@ -362,7 +362,7 @@ public:
 
 int main(int argc, char* argv[])
 {
-	constexpr int Ndim = 20001;
+	constexpr int Ndim = 100001;
 	constexpr double Tsi = 5.; // [scale * micrometer]
 	constexpr double KbT = 0.025851984732130292; //(ev)
 	constexpr double n_i = 1.5e10; // [cm^-3]
